19de722e77603bd32d08eb35061d7b49077689b2
jackandrea54 <piddfae@gmail.com>
2022-04-02 19:24:12 +0700

    Initial commit

---
+++ SS1/app.js
@@ -0,0 +1,51 @@
+let arr1 = [0,2,4,6,8]
+let arr2 = [1,3,5,7]
+
+
+// Cau A
+// for(let item of arr1){
+//     if(item > 3){
+//         console.log(item)
+//     }
+// }
+// for(let item of arr2){
+//     if(item > 3){
+//         console.log(item)
+//     }
+// }
+
+// Cau B
+// arr = []
+// for(let item of arr1)
+//     arr.push(item)
+// for(let item of arr2)
+//     arr.push(item)
+
+// console.log(arr)
+
+// Cau C
+
+arr = []
+for(let item of arr1)
+    arr.push(item)
+for(let item of arr2)
+    arr.push(item)
+
+
+do{
+    swapped = false
+    for(let i = 0; i < arr.length - 1; i++){
+        if (arr[i] > arr[i+1]){
+            swapped = true
+            var temp = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = temp
+        }
+    }
+}
+while (swapped)
+
+
+
+    
+console.log(arr.sort())
